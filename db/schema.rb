f2bede6af44b8d7c56b88fffaeb11a451f1ebe9c
Juan C. Müller <juan@challengepost.com>
2011-08-06 16:36:22 -0400

    YouTube auth!

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110806183800) do
+ActiveRecord::Schema.define(:version => 20110806202119) do
 
   create_table "users", :force => true do |t|
     t.string   "provider"
     t.string   "uid"
     t.string   "name"
-    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
