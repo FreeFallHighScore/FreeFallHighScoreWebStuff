dc593427d667f52fe5695a9f9abab4859af3c291
Juan C. Müller <juan@challengepost.com>
2011-08-06 13:29:05 -0400

    Initial commit with some code.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 This Will Be Awesome
 ==
+
+This project was started off of https://github.com/RailsApps/rails3-mongoid-omniauth
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+FreeFallHighScoreWebStuff::Application.config.session_store :cookie_store, :key => '_free-fall-high-score-web-stuff'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# FreeFallHighScoreWebStuff::Application.config.session_store :active_record_store
