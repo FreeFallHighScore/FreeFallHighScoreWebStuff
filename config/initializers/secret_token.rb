e15ba9c85c7dd682f4baa7281acaf0b1b51b8757
Juan C. Müller <juan@challengepost.com>
2011-08-06 14:38:28 -0400

    Rails 3.0.9 app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FreeFallHighScoreWebStuff::Application.config.secret_token = '7aadc7039a62a28ee3f0a21faf5b5b7709301bdb57d068c55e978b56607f060c8270f9cb6fd33a2924eb3d619bc3dc910dc262bf2d1ffb7dafeb5fa69b4c2efc'
