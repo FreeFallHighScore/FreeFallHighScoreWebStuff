0b851be2cf1534bc3f0958f36b0127cbd4897d66
Juan C. Müller <juan@challengepost.com>
2011-08-07 15:57:51 -0400

    Changes. Lots and lots of 'em.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def cities
+  end
+
+  def events
+  end
+
+  def lifevests
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 FreeFallHighScoreWebStuff::Application.routes.draw do
-  get "home/index"
-
   match "/auth/:provider/callback" => "sessions#create"
   match "/signout" => "sessions#destroy", :as => :signout
+
+  get "/cities" => "home#cities"
+  get "/lifevests" => "home#lifevests"
+  get "/events" => "home#events"
+  get "/disclaimer" => "home#disclaimer"
 
   root :to => "home#index"
 end
