a994b21d04f45775b1b824c69aae4a1d45c5dace
Juan C. Müller <juan@challengepost.com>
2011-08-08 17:13:11 -0400

    Fixin some stuff.

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ FreeFallHighScoreWebStuff::Application.routes.draw do
   match "/auth/:provider/callback" => "sessions#create"
   match "/signout" => "sessions#destroy", :as => :signout
 
-  get "/cities" => "home#cities"
-  get "/lifevests" => "home#lifevests"
-  get "/about" => "home#about"
+  get "/cities"     => "home#cities"
+  get "/lifevests"  => "home#lifevests"
+  get "/about"      => "home#about"
   get "/disclaimer" => "home#disclaimer"
 
   root :to => "home#index"
