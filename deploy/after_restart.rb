c50b48e4ed9e1a215b5b0bb3195afcd0306dcb2b
Juan C. Müller <juan@challengepost.com>
2011-09-07 20:48:00 -0400

    Add after restart deploy hook.

---
+++ deploy/after_restart.rb
@@ -0,0 +1,3 @@
+# This will have to be removed once the path is fixed.
+run "ln -s /var/www/FreeFallHighScoreWebStuff/current/public /var/www/FreeFallHighScoreWebStuff/current/public/staging_assets"
+run "ln -s /var/www/FreeFallHighScoreWebStuff/current/public /var/www/FreeFallHighScoreWebStuff/current/public/staging"
