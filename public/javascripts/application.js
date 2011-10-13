db8ec83a4dc6ab158205ecee2b82e3e2ed488849
Juan C. Müller <juan@challengepost.com>
2011-10-13 00:52:02 -0400

    Closurize JS...

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,13 +1,16 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-$(function() {
-  $('#content').infinitescroll({
-    // selector for the paged navigation (it will be hidden)
-    navSelector  : "div.pagination",
-    // selector for the NEXT link (to page 2)
-    nextSelector : "a.next_page",
-    // selector for all items you'll retrieve
-    itemSelector : "#content div.video"
-  });
-});
+var FFHS = {};
+(function() {
+  FFHS.infinitescroll = function(selector) {
+    $(selector).infinitescroll({
+      // selector for the paged navigation (it will be hidden)
+      navSelector  : "div.pagination",
+      // selector for the NEXT link (to page 2)
+      nextSelector : "a.next_page",
+      // selector for all items you'll retrieve
+      itemSelector : selector + " div.video"
+    });
+  }
+})($, FFHS);
