ca6b22cf5ed7e19843d65871fc57127284e92608
Juan C. Müller <juan@challengepost.com>
2011-10-13 00:53:45 -0400

    Revert "Closurize JS..."
    This reverts commit db8ec83a4dc6ab158205ecee2b82e3e2ed488849.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,16 +1,13 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-var FFHS = {};
-(function() {
-  FFHS.infinitescroll = function(selector) {
-    $(selector).infinitescroll({
-      // selector for the paged navigation (it will be hidden)
-      navSelector  : "div.pagination",
-      // selector for the NEXT link (to page 2)
-      nextSelector : "a.next_page",
-      // selector for all items you'll retrieve
-      itemSelector : selector + " div.video"
-    });
-  }
-})($, FFHS);
+$(function() {
+  $('#content').infinitescroll({
+    // selector for the paged navigation (it will be hidden)
+    navSelector  : "div.pagination",
+    // selector for the NEXT link (to page 2)
+    nextSelector : "a.next_page",
+    // selector for all items you'll retrieve
+    itemSelector : "#content div.video"
+  });
+});
